45f8ad8fc5b520fa1fe8147b1279421c17ebd30e
CarlosGutz <carlosgutz25@gmail.com>
2018-03-15 05:32:12 -0700

    added voting with winner testing

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -14,8 +14,18 @@ export function setEntries(state, entries) {
 
 Please note using the above funciton would also require edits to the core_spec.js file.
 See Note: """USING LIST VS ARRAY"""
-
 */
+
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+The below function was the initial next function but was modified to 
+better fit the voting needs of winners and losers.
+Instead of simply having a predictable next, our program has a slightly more
+robust next detection... now it can see entry A vs entry B and progress based on
+which had more votes!
 
 export function next(state){
     const entries = state.get('entries');
@@ -24,7 +34,62 @@ export function next(state){
         entries: entries.skip(2)
     });
 }
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
 /* merge()
 Returns a new Map resulting from merging the provided Collections into this new Map.
 Takes each entry of each collection and sets it on this new Map.
+
+https://facebook.github.io/immutable-js/docs/#/Map
+skip()
+Returns a new Collection of the same type which excludes the first amount entries from this Collection.
+skip(amount: number): this
 */
+
+export function vote( state, entry ) {
+    return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+    );
+}
+
+/*
+Exporting the next function will utilize the getWinners function to obtain the leading/winning
+Movie
+*/
+
+function getWinners( vote ) {
+    if( !vote ) {
+        return [];
+    }
+    
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+    
+    if( aVotes > bVotes ) {
+        return [a];
+    } else if( aVotes < bVotes ) {
+        return [b];
+    } else {
+        return [a, b];
+    };
+}
+
+export function next( state ) {
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
+    
+    if( entries.size == 1) {
+        return state.remove('vote').remove('entries').set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({ pair: entries.take(2) }),
+            entries: entries.skip(2)
+        });
+    }
+}
--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,6 +1,6 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
-import { setEntries, next } from '../src/core';
+import { setEntries, next, vote } from '../src/core';
 
 /*
 We have our immutable unit tests under immutable_spec.js
@@ -36,5 +36,119 @@ describe('application logic', () => {
                 entries: List.of('Sunshine')
             }));
         });
+
+        // This can be seen as a new phase, but still part of voting.
+        // Keep winner while discarding old ones. Ties end with keeping both.
+        it('puts winner of current vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+            
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting')
+            }));
+        });
+        // Incase a tie
+        it('places both from tied vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 3,
+                        '28 Days Later': 3
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+            }));
+        });        
+        // Ending vote because of winner!
+        it('marks winner when just one entry is left', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List()
+            });
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                winner: 'Trainspotting'
+            }));
+        });
+
     });
+    
+    // Vote tests will require phase of creation and phase of incrementing.
+    // Vote is ongoing. Verify votes are possible and tally increments successfully.
+    describe('vote', () => {
+        it('creates a tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Trainspotting');
+            
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 1
+                    })
+                }),
+                entries: List()
+            }));
+        });
+        // Phase of incrementing!
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 3,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List()
+            })
+            const nextState = vote(state, 'Trainspotting');
+            
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List()
+            }));
+        });
+
+    });
+
 });
